794544e2265aa5095aa8c38b7dc908dfe8232fc5
beliefdavid_360_window <beliefdavidsvn@gmail.com>
2023-03-23 14:46:31 +0900

    2023.03.23 - 자재 리스트 작업중 01

---
+++ src/main/java/com/jiretec/jirehManagement/controller/ProductManageController.java
@@ -0,0 +1,13 @@
+package com.jiretec.jirehManagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductManageController {
+
+    @GetMapping("/productList")
+    public String ProductList(){
+        return "/product/productList";
+    }
+}
